cc3bf6e4adeb596853ed56790bb0833d55769056
Anticopyright <53624153+dudul-bdul@users.noreply.github.com>
2020-05-14 02:04:53 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import _ from 'lodash'
 
 //endpoint
-export const url = "https://e-commerce-rest-api.herokuapp.com"
+export const url = "https://anti-app.netlify.app/"
 
 //headers
 export function headers(token) {
